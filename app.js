fa5ef931db4c6fc958ec2ea4527d6ca31f426235
SamuelPaschalson <109558179+SamuelPaschalson@users.noreply.github.com>
2024-12-01 15:23:36 +0100

    Update app.js

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ connectDB();
 const app = express();
 // app.use(express.json({ extended: true }));
 app.use(cors());
-app.use(bodyParser());
+// app.use(bodyParser());
 app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 app.use(express.json({ limit: '50mb', extended: true }));
 app.use(express.urlencoded({ limit: '50mb', extended: true }));
